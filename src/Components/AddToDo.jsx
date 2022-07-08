1ff57c94a0be9245561c9b22031b69b037d0cd70
Sean Martin <sean@seanmartinstudio.com>
2022-07-07 17:34:14 -0700

    Built Category Drop Down UI

--- src/Components/AddToDo.jsx
+++ src/Components/AddToDo.jsx
@@ -4,32 +4,47 @@ import { useState } from 'react'
 
 const AddToDo = () => {
 
+// State to create controlled form
 const [toDo, setToDo] = useState("")
 const [category, setCategory] = useState("")
 
+// To Do onChange
 const handleToDo = (event) => {
     event.preventDefault()
     setToDo(event.target.value)
-    // console.log("To Do =>", toDo)
+    console.log("To Do onChange =>", toDo)
 }
 
+// Category onChange
 const handleCategory = (event) => {
     setCategory(event.target.value)
-    // console.log("Category =>", category)
+    console.log("Category onChange =>", category)
 }
 
+// To Do and Category onSubmit (add API post here)
 const handleSubmit = (event) => {
     event.preventDefault()
-    console.log(toDo)
-    console.log(category)
+    console.log("To Do onSubmit =>", toDo)
+    console.log("Category onSubmit =>", category)
 }
 
   return (
     <div>
         <form onSubmit={handleSubmit} className="flex justify-center">
-            <textarea onChange={handleToDo} value={toDo} type='text' className="resize-none rounded-md border-solid border-2 border-black" ></textarea>
-            <textarea onChange={handleCategory} type='text'className="resize-none rounded-md border-solid border-2 border-black" ></textarea>
+
+            <input onChange={handleToDo} value={toDo} type='text' className="resize-none rounded-md border-solid border-2 border-black" ></input>
+
+            {/* <textarea onChange={handleCategory} type='text'className="resize-none rounded-md border-solid border-2 border-black" ></textarea> */}
+
+            <select onChange={handleCategory} name="categories" className="resize-none rounded-md border-solid border-2 border-black">
+                <option value="none">Categories</option>
+                <option value="Work">Work</option>
+                <option value="Personal">Personal</option>
+                <option name="Education">Education</option>
+            </select>
+
             <button type='submit' class="border-solid border-2 border-black rounded-md">Add</button>
+
         </form>
     </div>
   )
